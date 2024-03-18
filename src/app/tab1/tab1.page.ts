6445e436ea9b88ed301ed400c4ce24644c0cb408
gts.senna <gts.senna@gmail.com>
2024-03-18 03:39:31 -0300

    CHORE: add styles with checkbox

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { CheckboxChangeEventDetail, ModalController } from '@ionic/angular';
 import { TaskNameModalComponent } from '../components/task-name-modal/task-name-modal.component'
 
 interface ToDo{
+  id: number;
   description: string;
   done: boolean;
 }
@@ -13,7 +14,10 @@ interface ToDo{
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {  
-  list: ToDo[] = []
+  list: ToDo[] = [
+    {id: 1, description: "criar app", done: false},
+    {id: 2, description: "criar app 2", done: false}
+  ]
 
   constructor(
     private _modalCtrl: ModalController
@@ -29,8 +33,25 @@ export class Tab1Page {
     const { data, role } = await modal.onWillDismiss();
 
     if (role === 'confirm') {
-      this.list.push(data);
+      this.add(data);
     }
   }
 
+  add(description: string){
+    this.list.push({
+      id: this.list.length+1,
+      description,
+      done: false
+    })
+  }
+
+  changeItem(event: any){
+    this.list = this.list.map((task: ToDo ) => {
+      if(task.id == event.target.id)
+        return ({...task, done: event.target.checked})    
+
+      return task;      
+      }
+    )        
+  }
 }
